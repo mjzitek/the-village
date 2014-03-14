ac205c79d87384fd8b9419cc99028a2197596b70
Mike Zitek <mzitek@gmail.com>
2014-03-14 13:52:44 -0700

    Working on engine and models

--- core/server/models/db.js
+++ core/server/models/db.js
@@ -1,41 +1,18 @@
-var fs = require("fs");
-var file = "test.db";
-var exists = fs.existsSync(file);
 
-if(!exists) {
-  console.log("DB file not found");
-  //console.log("Creating DB file.");
-  //fs.openSync(file, "w");
-}
+	var mongoose = require('mongoose');
 
-var sqlite3 = require("sqlite3").verbose();
-var db = new sqlite3.Database(file);
+	mongoose.connect('mongodb://localhost/village');
 
-
-//var dbSelectStmt = db.prepare("SELECT username, email FROM users WHERE userId = 1");
-
-function User() {
-	this.username = null;
-	this.email = null;
-}
-
-var user1 = new User();
-
-var users = [];
-
-db.serialize(function() {
-	db.each("SELECT username, email FROM users", function (err, row) {
-		users.push(row);
+	var db = mongoose.connection;
+	db.on('error', console.error.bind(console, 'connection error:'));
+		db.once('open', function callback () {
+  		console.log('opened!');
 	});
 
-	console.log(users);
-});
-
-
-db.close();
 
 
 
 
 
-//module.exports = db;
+
+
