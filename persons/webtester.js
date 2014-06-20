00cd33ee4c1920b9431502bee340763e2c13149c
mjzitek <mzitek@gmail.com>
2014-06-20 11:40:31 -0700

    added graphing of grandchildren to persons webtester

--- persons/webtester.js
+++ persons/webtester.js
@@ -1,6 +1,7 @@
 
 var express = require('express'),
 	mongoose = require('mongoose'),
+	async = require('async'),
 	fs = require('fs');
 
 
@@ -130,51 +131,133 @@ app.get('/gameclock/', function(req, res) {
 app.get('/graphdata/:data', function(req, res) {
 	// var data = { name: "flare", children: [ { name: "cluster", 
 	// 				children: [{ name: "AgglomerativeCluster", size: 3938}] }]};
-  		console.log('Getting graph data...');
-	var data = {}
+  	console.log('Getting graph data...');
 
-  	persons.getPerson(req.params.data, function(per) {
-  		data['name'] = per.firstName + ' ' + per.lastName;
+  		getGraphData(req.params.data, function(data) {
+  			return res.jsonp(data);
+  		});			
 
 
-		
-		if(per.gender == "M")
-		{
-			console.log('Getting children...');
-			persons.getChildrenByFather(per._id, function(child) {
-				var children = [];
-				child.forEach(function(c) {
-					console.log('c: ' + c._id);
-					children.push({ 'name' : c.firstName + ' ' + c.lastName});
-				});
 
-				data['children'] = children;
-				console.log('xxyxyxyxyx');
-				console.log(data);
-				return res.jsonp(data)
-			});
-		} else if (per.gender == "F")
-		{
-			console.log('Getting children for ' + per._id);
-			persons.getChildrenByMother(per._id, function(child) {
-				console.log('Getting children...');
-				console.log(children);
-				var children = [];
-				child.forEach(function(c){ 
-					console.log(c);
-					children.push({ 'name' : c.firstName + ' ' + c.lastName, 'relation' : 'child'});
-				});
-
-				data['children'] = children;
-				console.log(data);
-				return res.jsonp(data);
-			});
-		} else {
-			return res.jsonp(data);
-		}
-
-    });
 });
+
+
+function getGraphData(personId, callback) {
+		var data = {}
+		var children = [];
+		console.log("Getting data for " + personId);
+		async.waterfall([
+			function(callback) {
+				  	persons.getPerson(personId, function(per) {
+				  		callback(null, per);
+				  	});
+			},
+			function(per, callback)
+			{
+				var data = {}
+				data['id'] = per._id;
+				data['name'] = per.firstName + ' ' + per.lastName;
+				callback(null, per, data)
+			},
+			function(person, data, callback)
+			{
+
+				if(person.gender == "M")
+				{
+					persons.getChildrenByFather(person._id, function(childern) {
+						// var children = [];
+						// child.forEach(function(c) {
+						// 	//console.log('c: ' + c._id);
+						// 	children.push({ 'id' : c._id, 'name' : c.firstName + ' ' + c.lastName});
+						// });
+						// data['children'] = children;
+						callback(null, person, children, data);
+					});
+				} else if (person.gender == "F")
+				{
+					persons.getChildrenByMother(person._id, function(children) {
+						//var children = [];
+						//child.forEach(function(c) {
+							//console.log('c: ' + c._id);
+							//children.push({ 'id' : c._id, 'name' : c.firstName + ' ' + c.lastName, 'gender' : c.gender});
+						//});
+						//console.log(children);
+						//data['children'] = children;
+						callback(null, person, children, data);
+					});
+				}
+			},
+			function(person, children, data, callback)
+			{
+				var waiting = 0;
+				var childrenArray = [];
+
+				children.forEach(function(c) {
+					waiting++;
+					var grandchildrenArray = [];
+					console.log("waiting++ " + waiting);
+					if(c.gender == "M")
+					{
+						persons.getChildrenByFather(c._id, function(gc) {
+							
+							gc.forEach(function(gcc) {
+								console.log('gc: ' + gcc._id);
+								grandchildrenArray.push({ 'id' : gcc._id, 'name' : gcc.firstName + ' ' + gcc.lastName, 'relation' : 'grandchild' });
+							});
+							waiting--;
+							console.log("waiting-- " + waiting);
+							childrenArray.push({ 'id' : c._id, 'name' : c.firstName + ' ' + c.lastName, 'gender' : c.gender, 
+												 'relation': 'child', 'children' : grandchildrenArray});
+
+							if(waiting == 0)
+							{
+								//console.log(data);
+								
+								//data['children'] = childrenArray;
+								console.log(childrenArray);
+								callback(null, person, childrenArray, data);
+							}
+
+						});
+					} else if (c.gender == "F")
+					{
+						persons.getChildrenByMother(c._id, function(gc) {
+							
+							gc.forEach(function(gcc) {
+								console.log('gc: ' + gcc._id);
+								grandchildrenArray.push({ 'id' : gcc._id, 'name' : gcc.firstName + ' ' + gcc.lastName, 'relation' : 'grandchild' });
+							});
+							waiting--;
+							console.log("waiting-- " + waiting);
+								childrenArray.push({ 'id' : c._id, 'name' : c.firstName + ' ' + c.lastName, 'gender' : c.gender, 
+												     'relation': 'child', 'children' : grandchildrenArray });
+							if(waiting == 0)
+							{
+								//console.log(data);
+
+								//data['children'] = childrenArray;
+								console.log(childrenArray);
+								callback(null, person, childrenArray, data);
+							}
+
+						});
+					}
+				});
+
+			}
+		],
+		function(err, per, childrenArray, data) {
+			data['children'] = childrenArray;
+	    	if(err) {
+	    		callback(err);
+	    	} else
+	    	{
+	    		callback(data);    		
+	    	}
+
+		});
+
+}
 
 /////////////////////////
 var server = app.listen(8989, function() {
