fcba89ae8466ef076f11c78f2ee238d46739c77a
Michael Zitek <mzitek@gmail.com>
2014-08-20 09:41:49 -0700

    Adding FoodItems schema

---
+++ persons/models/FoodItems.js
@@ -0,0 +1,25 @@
+var mongoose = require('mongoose'),
+    Schema = mongoose.Schema;
+
+var FoodItemSchema = new mongoose.Schema({
+		name:        String,
+		nameFormal:  String,
+		portions:    Number
+		stats:  {
+					hunger: 			Number,
+					thirst:				Number,
+					stamina: 			Number,
+					hungerSaturation: 	Number,
+					happiness: 			Number	
+				},
+		cooked:      Boolean,
+		complex:     Boolean,
+		ingredients: [
+				{
+					foodItem: { type: Schema.Types.ObjectId, ref: 'fooditems' },
+					quantity: Number
+				}
+			]
+});
+
+mongoose.model('fooditems', FoodItems);
