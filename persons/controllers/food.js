744b6d000bbdaa826de7ce59f113d70d117da9f9
Michael Zitek <mzitek@gmail.com>
2014-08-22 11:47:18 -0700

    adding food handling controller

---
+++ persons/controllers/food.js
@@ -0,0 +1,19 @@
+/*
+
+	This controller is for the handling of food items as
+	related to inventories, doing things with food, etc
+	where as the fooditem controller is primarily for interfacing
+	with the FoodItems collection
+
+*/
+
+
+function addToVillagersInventory() {}
+
+function removeFromVillagersInventory() {}
+
+function eatFoodItem() {}
+
+function addToStructuresInventory() {}
+
+function removeFromStructuresInventory() {}
